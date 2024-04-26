1a944d4eec4e7fc3dd219319af53b2be359871b3
Andrew Reasoa <drw026@gmail.com>
2024-04-26 20:59:07 +0200

    Add ShowDetails and ShowOverview

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Overview from './pages/Overview.vue';
 import Details from './pages/Details.vue';
+import ShowOverview from './components/ShowOverview/ShowOverview.vue';
+import ShowDetails from './components/ShowDetails/ShowDetails.vue';
 
 const routes = [
-  { path: '/', component: Overview, children: [{ path: '', component: MovieOverview }] },
-  { path: '/', component: Details, children: [{ path: '', component: MovieDetail }] },
+  { path: '/', component: Overview, children: [{ path: '', component: ShowOverview }] },
+  { path: '/show/:id', component: Details, children: [{ path: '', component: ShowDetails }] },
 ];
 
 const router = createRouter({
