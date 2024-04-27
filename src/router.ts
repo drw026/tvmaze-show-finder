93939e731b34679a0e78b433b3a5471ffe6e9356
Andrew Reasoa <drw026@gmail.com>
2024-04-27 16:00:49 +0200

    Add Components in route

--- src/router.ts
+++ src/router.ts
@@ -3,6 +3,8 @@ import Overview from './pages/Overview.vue';
 import Details from './pages/Details.vue';
 import ShowOverview from './components/ShowOverview/ShowOverview.vue';
 import ShowDetails from './components/ShowDetails/ShowDetails.vue';
+import SearchOverview from './components/SearchOverview/SearchOverview.vue';
+import Search from './pages/Search.vue';
 
 const routes = [
   { path: '/', component: Overview, children: [{ path: '', component: ShowOverview }] },
