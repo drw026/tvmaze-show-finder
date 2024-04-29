0ff703388277a6313e1d3d4e99a1bf42136b4f0b
Andrew Reasoa <drw026@gmail.com>
2024-04-29 15:40:18 +0200

    Create constant for number of shows per genre

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,5 +1,7 @@
 const { VITE_TVMAZE_API_URL } = import.meta.env;
 
+export const NUMBER_OF_SHOWS_PER_GENRE = 10;
+
 export {
   VITE_TVMAZE_API_URL
 }
--- src/lib/mappers/mapShows.ts
+++ src/lib/mappers/mapShows.ts
@@ -1,35 +1,43 @@
 import { ShowResponse, ShowsInGenre } from '../../types/Show';
+import { NUMBER_OF_SHOWS_PER_GENRE } from '../../common/constants';
 
 export function mapShows(data: ShowResponse[]): ShowsInGenre[] {
   const allGenres = data.reduce((allGenre, show) => {
-    show.genres.forEach(genre => allGenre.add(genre));
+    show.genres.forEach((genre) => allGenre.add(genre));
     return allGenre;
   }, new Set<string>());
 
-  return [...allGenres].map((genre) => {
-    const shows = data
-      .filter((show) => show.genres.includes(genre))
-      .sort((a, b) => b.rating.average - a.rating.average)
-      .map(({id, name, image}) => {
-        return {
-          id,
-          name: name || '',
-          imageUrl: image.original || image.medium || ''}
-      })
+  return [...allGenres]
+    .map((genre) => {
+      const shows = data
+        .filter((show) => show.genres.includes(genre))
+        .sort((a, b) => b.rating.average - a.rating.average)
+        .map(({ id, name, image }) => {
+          return {
+            id,
+            name: name || '',
+            imageUrl: image?.original || image?.medium || '',
+          };
+        });
 
-    return {
-      genreName: genre,
-      shows: shows.length > 10 ? shows.splice(0, 10) : shows
-    };
-  }).sort((a, b) => {
-    const stringA = a.genreName.toUpperCase();
-    const stringB = b.genreName.toUpperCase();
-    if (stringA < stringB) {
-      return -1;
-    }
-    if (stringA > stringB) {
-      return 1;
-    }
-    return 0;
-  })
+      return {
+        genreName: genre,
+        shows:
+          shows.length > NUMBER_OF_SHOWS_PER_GENRE
+            ? shows.splice(0, NUMBER_OF_SHOWS_PER_GENRE)
+            : shows,
+      };
+    })
+    .sort((a, b) => {
+      // sort Genres alphabetically
+      const stringA = a.genreName.toUpperCase();
+      const stringB = b.genreName.toUpperCase();
+      if (stringA < stringB) {
+        return -1;
+      }
+      if (stringA > stringB) {
+        return 1;
+      }
+      return 0;
+    });
 }
