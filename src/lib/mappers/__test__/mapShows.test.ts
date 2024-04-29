56d867d50a020482424b75fba75ecb78f137645e
Andrew Reasoa <drw026@gmail.com>
2024-04-29 21:12:36 +0200

    test mapShows

---
+++ src/lib/mappers/__test__/mapShows.test.ts
@@ -0,0 +1,12 @@
+import { expect, it, describe } from 'vitest';
+import { mapShows } from '../mapShows'
+import { mockShowsResponse } from '../../../mocks/mockShowsResponse';
+import { mockShowsInGenre } from '../../../mocks/mockShowsInGenre';
+import { ShowResponse } from '../../../types/Show';
+
+describe('mapShows', () => {
+  it('should map the raw data', () => {
+    expect(mapShows(mockShowsResponse as unknown as ShowResponse[])).toEqual(mockShowsInGenre)
+  })
+});
+
---
+++ src/mocks/mockShowsInGenre.ts
@@ -0,0 +1,212 @@
+export const mockShowsInGenre = [
+  {
+    genreName: "Action",
+    shows: [
+      {
+        id: 2,
+        name: "Person of Interest",
+        imageUrl: "https://static.tvmaze.com/uploads/images/original_untouched/163/407679.jpg"
+      },
+      {
+        id: 6,
+        name: "The 100",
+        imageUrl: "https://static.tvmaze.com/uploads/images/original_untouched/477/1194981.jpg"
+      },
+      {
+        id: 4,
+        name: "Arrow",
+        imageUrl: "https://static.tvmaze.com/uploads/images/original_untouched/143/358967.jpg"
+      }
+    ]
+  },
+  {
+    genreName: "Adventure",
+    shows: [
+      {
+        id: 6,
+        name: "The 100",
+        imageUrl: "https://static.tvmaze.com/uploads/images/original_untouched/477/1194981.jpg"
+      }
+    ]
+  },
+  {
+    genreName: "Crime",
+    shows: [
+      {
+        id: 2,
+        name: "Person of Interest",
+        imageUrl: "https://static.tvmaze.com/uploads/images/original_untouched/163/407679.jpg"
+      },
+      {
+        id: 10,
+        name: "Grimm",
+        imageUrl: "https://static.tvmaze.com/uploads/images/original_untouched/69/174906.jpg"
+      },
+      {
+        id: 5,
+        name: "True Detective",
+        imageUrl: "https://static.tvmaze.com/uploads/images/original_untouched/490/1226764.jpg"
+      }
+    ]
+  },
+  {
+    genreName: "Drama",
+    shows: [
+      {
+        id: 10,
+        name: "Grimm",
+        imageUrl: "https://static.tvmaze.com/uploads/images/original_untouched/69/174906.jpg"
+      },
+      {
+        id: 5,
+        name: "True Detective",
+        imageUrl: "https://static.tvmaze.com/uploads/images/original_untouched/490/1226764.jpg"
+      },
+      {
+        id: 7,
+        name: "Homeland",
+        imageUrl: "https://static.tvmaze.com/uploads/images/original_untouched/498/1245275.jpg"
+      },
+      {
+        id: 9,
+        name: "Revenge",
+        imageUrl: "https://static.tvmaze.com/uploads/images/original_untouched/82/206879.jpg"
+      },
+      {
+        id: 3,
+        name: "Bitten",
+        imageUrl: "https://static.tvmaze.com/uploads/images/original_untouched/0/15.jpg"
+      },
+      {
+        id: 4,
+        name: "Arrow",
+        imageUrl: "https://static.tvmaze.com/uploads/images/original_untouched/143/358967.jpg"
+      },
+      {
+        id: 1,
+        name: "Under the Dome",
+        imageUrl: "https://static.tvmaze.com/uploads/images/original_untouched/81/202627.jpg"
+      },
+      {
+        id: 8,
+        name: "Glee",
+        imageUrl: "https://static.tvmaze.com/uploads/images/original_untouched/0/73.jpg"
+      }
+    ]
+  },
+  {
+    genreName: "Espionage",
+    shows: [
+      {
+        id: 7,
+        name: "Homeland",
+        imageUrl: "https://static.tvmaze.com/uploads/images/original_untouched/498/1245275.jpg"
+      }
+    ]
+  },
+  {
+    genreName: "Horror",
+    shows: [
+      {
+        id: 3,
+        name: "Bitten",
+        imageUrl: "https://static.tvmaze.com/uploads/images/original_untouched/0/15.jpg"
+      }
+    ]
+  },
+  {
+    genreName: "Music",
+    shows: [
+      {
+        id: 8,
+        name: "Glee",
+        imageUrl: "https://static.tvmaze.com/uploads/images/original_untouched/0/73.jpg"
+      }
+    ]
+  },
+  {
+    genreName: "Mystery",
+    shows: [
+      {
+        id: 9,
+        name: "Revenge",
+        imageUrl: "https://static.tvmaze.com/uploads/images/original_untouched/82/206879.jpg"
+      }
+    ]
+  },
+  {
+    genreName: "Romance",
+    shows: [
+      {
+        id: 3,
+        name: "Bitten",
+        imageUrl: "https://static.tvmaze.com/uploads/images/original_untouched/0/15.jpg"
+      },
+      {
+        id: 8,
+        name: "Glee",
+        imageUrl: "https://static.tvmaze.com/uploads/images/original_untouched/0/73.jpg"
+      }
+    ]
+  },
+  {
+    genreName: "Science-Fiction",
+    shows: [
+      {
+        id: 2,
+        name: "Person of Interest",
+        imageUrl: "https://static.tvmaze.com/uploads/images/original_untouched/163/407679.jpg"
+      },
+      {
+        id: 6,
+        name: "The 100",
+        imageUrl: "https://static.tvmaze.com/uploads/images/original_untouched/477/1194981.jpg"
+      },
+      {
+        id: 4,
+        name: "Arrow",
+        imageUrl: "https://static.tvmaze.com/uploads/images/original_untouched/143/358967.jpg"
+      },
+      {
+        id: 1,
+        name: "Under the Dome",
+        imageUrl: "https://static.tvmaze.com/uploads/images/original_untouched/81/202627.jpg"
+      }
+    ]
+  },
+  {
+    genreName: "Supernatural",
+    shows: [
+      {
+        id: 10,
+        name: "Grimm",
+        imageUrl: "https://static.tvmaze.com/uploads/images/original_untouched/69/174906.jpg"
+      }
+    ]
+  },
+  {
+    genreName: "Thriller",
+    shows: [
+      {
+        id: 5,
+        name: "True Detective",
+        imageUrl: "https://static.tvmaze.com/uploads/images/original_untouched/490/1226764.jpg"
+      },
+      {
+        id: 7,
+        name: "Homeland",
+        imageUrl: "https://static.tvmaze.com/uploads/images/original_untouched/498/1245275.jpg"
+      },
+      {
+        id: 9,
+        name: "Revenge",
+        imageUrl: "https://static.tvmaze.com/uploads/images/original_untouched/82/206879.jpg"
+      },
+      {
+        id: 1,
+        name: "Under the Dome",
+        imageUrl: "https://static.tvmaze.com/uploads/images/original_untouched/81/202627.jpg"
+      }
+    ]
+  }
+]
