fa6453af39e6dcee7e95325ffe43df62c82c2f39
Andrew Reasoa <drw026@gmail.com>
2024-04-30 22:41:28 +0200

    test show overview

---
+++ src/components/ShowOverview/__tests__/ShowOverview.test.ts
@@ -0,0 +1,64 @@
+import { vi, it, describe, expect } from 'vitest';
+import { shallowMount, mount } from '@vue/test-utils';
+import ShowOverview from '../ShowOverview.vue';
+import { ShowsInGenre } from '../../../types/Show';
+
+const mockUseShows = vi.hoisted(
+  (): { isLoading: boolean; data: ShowsInGenre[] | null } => ({
+    isLoading: false,
+    data: [],
+  }),
+);
+
+vi.mock('../../../lib/services/useShows', () => ({
+  useShows: () => {
+    return {
+      isLoading: mockUseShows.isLoading,
+      data: mockUseShows.data,
+    };
+  },
+}));
+
+describe('ShowOverview', () => {
+  it('should render without crashing', () => {
+    mount(ShowOverview);
+  });
+
+  it('should show loading spinner when loading', () => {
+    mockUseShows.isLoading = true;
+
+    const wrapper = mount(ShowOverview);
+
+    expect(wrapper.text()).toContain('Loading...');
+  });
+
+  it('should show "no data" when not loading and data is empty', () => {
+    mockUseShows.isLoading = false;
+    mockUseShows.data = null;
+
+    const wrapper = shallowMount(ShowOverview);
+
+    expect(wrapper.text()).toContain('No data');
+  });
+
+  it('should show data genre with a show card', () => {
+    mockUseShows.isLoading = false;
+    mockUseShows.data = [
+      {
+        genreName: 'Genre',
+        shows: [
+          {
+            id: 1,
+            name: 'Show',
+            imageUrl: 'imageUrl',
+          },
+        ],
+      },
+    ];
+
+    const wrapper = shallowMount(ShowOverview);
+    const genre = wrapper.find('h2');
+
+    expect(genre.text()).toBe('Genre');
+  });
+});
