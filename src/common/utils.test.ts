a87df8401a50f4f23f594da7bdc41594b12a8211
Andrew Reasoa <drw026@gmail.com>
2024-05-01 19:58:12 +0200

    test utils

---
+++ src/common/utils.test.ts
@@ -0,0 +1,45 @@
+import { sleep, timer } from './utils';
+import { it, describe, expect } from 'vitest';
+
+describe('timer', () => {
+  const timeStamp = timer();
+
+  it('start method should set the timestamp to the current time', async () => {
+    const startTime = new Date().getTime();
+    timeStamp.start();
+    const timeElapsed = timeStamp.stop();
+
+    expect(timeElapsed).toBeGreaterThanOrEqual(0);
+    expect(timeElapsed).toBeLessThan(startTime);
+  });
+
+  it('stop method should return elapsed time', async () => {
+    timeStamp.start();
+    const sleepMs = 3000;
+    await new Promise<void>((resolve) => setTimeout(() => resolve(), sleepMs));
+    const timeElapsed = timeStamp.stop();
+
+    // The elapsed time should be at least as long as we slept
+    expect(timeElapsed).toBeGreaterThanOrEqual(sleepMs);
+  });
+});
+
+describe('sleep', () => {
+  it('should delay the execution of the subsequent code for the specified time', async () => {
+    const delayMs = 500;
+    const startTime = Date.now();
+    await sleep(delayMs);
+    const endTime = Date.now();
+
+    expect(endTime - startTime).toBeGreaterThanOrEqual(delayMs);
+  });
+
+  it('should resolve without delaying when passed a non-positive time', async () => {
+    const delayMs = -500;
+    const startTime = Date.now();
+    await sleep(delayMs);
+    const endTime = Date.now();
+
+    expect(endTime - startTime).toBeLessThan(10); // execution should be near-instant
+  });
+});
