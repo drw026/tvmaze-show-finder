2c0219f2cf5dabaa4e605eaf4d1cccc9c84453de
Andrew Reasoa <drw026@gmail.com>
2024-05-01 22:10:44 +0200

    add search icon to search button

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,7 +1,10 @@
 <template>
-  <div class="flex gap-3">
-    <button @click="search" class="rounded bg-yellow px-3 py-2 font-bold hover:bg-yellow-light">
-      Search
+  <div class="flex flex-1 gap-3">
+    <button @click="search" class="flex gap-1 rounded bg-yellow px-3 py-2 font-bold hover:bg-yellow-light">
+      <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+        <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+      </svg>
+      <span class="hidden sm:flex">Search</span>
     </button>
     <input
       type="search"
