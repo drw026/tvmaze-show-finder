55b876c2db150d88482d280ea3d2e815555246ee
Andrew Reasoa <drw026@gmail.com>
2024-05-01 16:17:24 +0200

    test Search

---
+++ src/components/__tests__/Search.test.ts
@@ -0,0 +1,27 @@
+import { describe, it, expect } from 'vitest';
+import { mount } from '@vue/test-utils';
+import Search from '../Search.vue';
+import router from '../../router';
+
+describe('Search component', () => {
+  it('should render without crashing', () => {
+    mount(Search, {
+      global: {
+        plugins: [router]
+      },
+    });
+  });
+
+  it('should render button and empty input', () => {
+    const wrapper = mount(Search, {
+      global: {
+        plugins: [router]
+      },
+    });
+    const button = wrapper.find('button');
+    const input = wrapper.find('input');
+
+    expect(button.text()).toBe('Search');
+    expect(input.element.value).toBe('');
+  });
+});
